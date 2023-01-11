6fdb3c02ef55b3ba5a42eb84e0d6ad1977f3a540
José Simões <jose.simoes@eclo.solutions>
2023-01-11 18:56:18 +0000

    Fix GPIO config

--- targets/AzureRTOS/ST/ORGPAL_PALTHREE/common/CubeMX/target_hal_gpio.c
+++ targets/AzureRTOS/ST/ORGPAL_PALTHREE/common/CubeMX/target_hal_gpio.c
@@ -10,12 +10,10 @@ void MX_GPIO_Init(void)
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
     // GPIO Ports Clock Enable
-    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
     __HAL_RCC_GPIOG_CLK_ENABLE();
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    __HAL_RCC_GPIOI_CLK_ENABLE();
-    __HAL_RCC_GPIOH_CLK_ENABLE();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOJ_CLK_ENABLE();
+    __HAL_RCC_GPIOK_CLK_ENABLE();
 
     // Configure GPIO pins Output Level
     HAL_GPIO_WritePin(GPIOG, GPIO_PIN_7, GPIO_PIN_RESET);
