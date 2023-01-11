c009744c3d1fcdb42c91afbb8740811e07c3f1d9
José Simões <jose.simoes@eclo.solutions>
2023-01-11 18:56:30 +0000

    Fix PLL config

--- targets/AzureRTOS/ST/ORGPAL_PALTHREE/common/CubeMX/stm32f7xx_hal_msp.c
+++ targets/AzureRTOS/ST/ORGPAL_PALTHREE/common/CubeMX/stm32f7xx_hal_msp.c
@@ -323,9 +323,9 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
     RCC_OscInitStruct.PLL.PLLM = 6;
-    RCC_OscInitStruct.PLL.PLLN = 200;
+    RCC_OscInitStruct.PLL.PLLN = 216;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = 8;
+    RCC_OscInitStruct.PLL.PLLQ = 9;
     RCC_OscInitStruct.PLL.PLLR = 2;
 
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
