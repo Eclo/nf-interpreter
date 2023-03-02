97d1fa41454fa904f0828355d82aff0cde65d9f1
José Simões <jose.simoes@eclo.solutions>
2023-03-02 17:57:20 +0000

    More work on adding ST_STM32H735G_DK

---
+++ targets/AzureRTOS/ST/ST_STM32H735G_DK/common/target_hal_gpio.c
@@ -0,0 +1,19 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+#include <stm32h7xx_hal.h>
+
+void MX_GPIO_Init(void)
+{
+    //GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    // GPIO Ports Clock Enable
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOI_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+}
