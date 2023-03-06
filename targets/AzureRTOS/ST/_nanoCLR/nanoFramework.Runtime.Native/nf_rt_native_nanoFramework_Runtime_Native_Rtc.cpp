b9a7a47dc75130e81d990a1bcfe5bff2e5d8e961
José Simões <jose.simoes@eclo.solutions>
2023-03-06 16:54:24 +0000

    Fix include of STM32H7 in runtime native rtc

--- targets/AzureRTOS/ST/_nanoCLR/nanoFramework.Runtime.Native/nf_rt_native_nanoFramework_Runtime_Native_Rtc.cpp
+++ targets/AzureRTOS/ST/_nanoCLR/nanoFramework.Runtime.Native/nf_rt_native_nanoFramework_Runtime_Native_Rtc.cpp
@@ -3,7 +3,13 @@
 // See LICENSE file in the project root for full license information.
 //
 
+#ifdef STM32H7XX
+#include <stm32h7xx_hal.h>
+#endif
+#ifdef STM32F7XX
 #include <stm32f7xx_hal.h>
+#endif
+
 #include <nf_rt_native.h>
 
 //////////////////////////////////////////////////////////////////////////////////
